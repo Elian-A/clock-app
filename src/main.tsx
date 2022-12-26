e47ceaf68b49f4cf79246893ad1d3ffda931c4f1
Elian Avalos <elianemanuelstudy@gmail.com>
2022-12-26 01:01:02 -0300

    Installtailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css'
 
 import { QueryClient, QueryClientProvider } from 'react-query'
 
@@ -9,7 +10,6 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-
       <App />
     </QueryClientProvider>
   </React.StrictMode>,
