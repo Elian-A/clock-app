67dc96da39e630475947f6409d1772115d169be0
Elian Avalos <elianemanuelstudy@gmail.com>
2022-12-26 22:08:32 -0300

    Move infoList into utils

--- src/components/infoList/utils.ts
+++ src/components/infoList/utils.ts
@@ -1,7 +1,7 @@
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-export const dayOfYear = (today: Date) => {
+const dayOfYear = (today: Date) => {
     // Get the first day of a year
     let firstDay = new Date(today.getFullYear(), 0);
 
@@ -10,5 +10,27 @@ export const dayOfYear = (today: Date) => {
 
     return dayOfYear
 }
-export const day = (today: Date) => days[today.getDay()]
-export const month = (today: Date) => months[today.getMonth()]
+const day = (today: Date) => days[today.getDay()]
+const month = (today: Date) => months[today.getMonth()]
+export const getInfoList = (today: Date) => [
+    {
+      id: 1,
+      title: "Current timezone",
+      data: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    },
+    {
+      id: 2,
+      title: "Day of the year",
+      data: dayOfYear(today),
+    },
+    {
+      id: 3,
+      title: "Day of the week",
+      data: day(today),
+    },
+    {
+      id: 4,
+      title: "Month",
+      data: month(today),
+    },
+  ]
