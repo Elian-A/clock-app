dd71e2e48cecb376ef57a52a2b68ad4c51b31105
Elian Avalos <elianemanuelstudy@gmail.com>
2022-12-27 01:44:27 -0300

    Move static svg files to components

---
+++ src/components/SVGs/index.ts
@@ -0,0 +1,3 @@
+export { default as Moon } from "./moonSVG";
+export { default as Sun } from "./sunSVG";
+export { default as Reload } from "./reloadSVG";
---
+++ src/components/SVGs/moonSVG.tsx
@@ -0,0 +1,18 @@
+const MoonSVG = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1rem"
+      height="1rem"
+      viewBox="0 0 24 24"
+      className="scale-150"
+    >
+      <path
+        fill="white"
+        d="M12 2a9.91 9.91 0 0 0-3 .46a10 10 0 0 1 0 19.08A10 10 0 1 0 12 2Z"
+      />
+    </svg>
+  );
+};
+
+export default MoonSVG;
