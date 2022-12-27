c4391186828c19e98fdeb3204d496d752fa2e974
Elian Avalos <elianemanuelstudy@gmail.com>
2022-12-27 15:31:59 -0300

    Fixed typo

--- src/components/SVGs/greaterThenSVG.tsx
+++ src/components/SVGs/greaterThenSVG.tsx
@@ -9,7 +9,7 @@ const GreaterThen = () => {
     >
       <path
         fill="currentColor"
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="m7.25 17l7.5-5l-7.5-5a.901.901 0 1 1 1-1.5l8.502 5.668a1 1 0 0 1 0 1.664L8.25 18.5a.901.901 0 1 1-1-1.5z"
       />
     </svg>
