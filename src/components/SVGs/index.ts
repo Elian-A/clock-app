4c680852ee6683df93b8a45149f538e0ffa2fc92
Elian Avalos <elianemanuelstudy@gmail.com>
2022-12-27 12:31:52 -0300

    Added greaterThen SVG

--- src/components/SVGs/index.ts
+++ src/components/SVGs/index.ts
@@ -1,3 +1,4 @@
 export { default as Moon } from "./moonSVG";
 export { default as Sun } from "./sunSVG";
 export { default as Reload } from "./reloadSVG";
+export { default as GreaterThen } from "./greaterThenSVG";
