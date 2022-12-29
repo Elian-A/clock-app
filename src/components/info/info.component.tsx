38cdb877d206970520028578b216ff0b2d39caf3
Elian Avalos <elianemanuelstudy@gmail.com>
2022-12-29 00:26:59 -0300

    Styled info components

--- src/components/info/info.component.tsx
+++ src/components/info/info.component.tsx
@@ -1,18 +1,19 @@
-import { FC } from "react"
+import { FC } from "react";
 
 interface Info {
-    id: number,
-    title: string,
-    data: string | number
+  id: number;
+  title: string;
+  data: string | number;
 }
 
 const Info: FC<{ info: Info }> = ({ info }) => {
-    const { data, title } = info
-    return (
-        <div>
-            <p>{title}</p><p>{data}</p>
-        </div>
-    )
-}
+  const { data, title } = info;
+  return (
+    <div className="flex justify-between items-center pb-4 last:pb-0">
+      <p className="font-serif uppercase text-gray-400">{title}</p>
+      <p className="font-sans font-bold text-white">{data}</p>
+    </div>
+  );
+};
 
-export default Info
+export default Info;
--- src/components/infoList/infoList.component.tsx
+++ src/components/infoList/infoList.component.tsx
@@ -5,7 +5,10 @@ import { getInfoList } from "./infoList.utils";
 const InfoList: FC<{ date: Date }> = ({ date }) => {
   const infoList = getInfoList(date);
   return (
-    <div>
+    <div
+      className="flex flex-col gap-2 blur-[.3px] px-5 py-10"
+      style={{ backgroundColor: "rgba(0 0 0 / .7)" }}
+    >
       {infoList.map((info) => (
         <Info key={info.id} info={info} />
       ))}
