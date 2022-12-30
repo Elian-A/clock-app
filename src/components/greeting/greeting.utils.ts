434593cffb25916a439729ea1d1423e7c5779250
Elian Avalos <elianemanuelstudy@gmail.com>
2022-12-30 08:55:11 -0300

    Fixed minor bug

--- src/components/greeting/greeting.utils.ts
+++ src/components/greeting/greeting.utils.ts
@@ -1,5 +1,5 @@
 export const greet = (hours: number) => {
-    const greeting = +hours > 6 && +hours < 12 ? "Good Morning"
-        : +hours > 12 && +hours < 18 ? "Good Afternoon" : "Good Night"
+    const greeting = +hours >= 6 && +hours < 12 ? "Good Morning"
+        : +hours >= 12 && +hours < 18 ? "Good Afternoon" : "Good Night"
     return greeting
 }
